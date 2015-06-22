f1665b044057f14af3fd5824ef7eef12fd0afd56
Rely Ky <relyky@hotmail.com>
2015-06-22 22:57:27 +0800

    First

---
+++ httpWebRequestLab/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace httpWebRequestLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlToolbar = new System.Windows.Forms.Panel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.pnlToolbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlToolbar
+            // 
+            this.pnlToolbar.Controls.Add(this.txtUrl);
+            this.pnlToolbar.Controls.Add(this.label1);
+            this.pnlToolbar.Controls.Add(this.btnGo);
+            this.pnlToolbar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlToolbar.Location = new System.Drawing.Point(0, 0);
+            this.pnlToolbar.Name = "pnlToolbar";
+            this.pnlToolbar.Size = new System.Drawing.Size(645, 88);
+            this.pnlToolbar.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(60, 28);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(291, 22);
+            this.txtUrl.TabIndex = 2;
+            this.txtUrl.Text = "http://www.google.com";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(385, 26);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtMessage.Font = new System.Drawing.Font("細明體-ExtB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtMessage.Location = new System.Drawing.Point(0, 376);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(645, 102);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Font = new System.Drawing.Font("細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(645, 376);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 478);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.pnlToolbar);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "HttpWebRequest Lab";
+            this.pnlToolbar.ResumeLayout(false);
+            this.pnlToolbar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlToolbar;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ httpWebRequestLab/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace httpWebRequestLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //# prefix actions
+                txtOutput.Clear();
+
+                //
+                string url = txtUrl.Text.Trim();
+                txtMessage.AppendText("\r\n### ### ### ### ### ###\r\n");
+                txtMessage.AppendText(string.Format("URL : {0}\r\n", url));
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                // Set some reasonable limits on resources used by this request
+                request.MaximumAutomaticRedirections = 4;
+                request.MaximumResponseHeadersLength = 4;
+                request.Timeout = 30000; // 30 seconds
+
+                // Set credentials to use for this request.
+                request.Credentials = CredentialCache.DefaultCredentials;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                txtMessage.AppendText(string.Format("Content Length : {0}\r\n", response.ContentLength));
+                txtMessage.AppendText(string.Format("Content Type : {0}\r\n", response.ContentType));
+                txtMessage.AppendText(string.Format("Content Encoding : {0}\r\n", response.ContentEncoding));
+
+                // Get the stream associated with the response.
+                //Stream receiveStream = response.GetResponseStream();
+
+                // Pipes the stream to a higher level stream reader with the required encoding format. 
+                StreamReader readStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                txtMessage.AppendText(string.Format("Response stream received."));
+
+                // 
+                txtOutput.AppendText(readStream.ReadToEnd());
+
+                response.Close();
+                readStream.Close();                    
+            }
+            catch(Exception ex)
+            {
+                txtMessage.AppendText("\r\n ===>>><<<===\r\n");
+                txtMessage.AppendText(string.Format("EXCEPTION : {0}\r\n", ex.GetType().Name));
+                txtMessage.AppendText(string.Format("MESSAGE : {0}\r\n", ex.Message));
+            }
+
+        }
+    }
+}
