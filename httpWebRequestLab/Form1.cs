fde5dd8ea708cb3fbf5eb1ce0d73e71d818b18ef
Rely Ky <relyky@hotmail.com>
2015-06-23 15:20:31 +0800

    fix some

--- httpWebRequestLab/Form1.cs
+++ httpWebRequestLab/Form1.cs
@@ -13,6 +13,7 @@ using System.Text.RegularExpressions;
 using System.Net.Security;
 using System.Collections;
 using NCCUCS.AspectW;
+using System.Runtime.CompilerServices; 
 
 namespace httpWebRequestLab
 {
@@ -38,9 +39,12 @@ namespace httpWebRequestLab
             cboUrl.SelectedIndex = 0;
         }
 
+        #region Aspect Program
+
         /// <summary>
         /// One of Aspect Function
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void TraceExceptionHanler(Exception ex)
         {
             txtMessage.AppendText("\r\n ===>>><<<===\r\n");
@@ -51,20 +55,25 @@ namespace httpWebRequestLab
         /// <summary>
         /// One of Aspect Function
         /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void TraceHowLongHanler(TimeSpan ts)
         {
             txtMessage.AppendText(string.Format("SPAND : {0} seconds\r\n", ts.TotalSeconds));
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void TraceLine(string msg)
         {
             txtMessage.AppendText(msg + "\r\n");
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void TraceLine(string format, params object[] args)
         {
             txtMessage.AppendText(string.Format(format + "\r\n", args));
         }
+
+        #endregion
 
         private void btnGo_Click(object sender, EventArgs e)
         {
@@ -119,64 +128,5 @@ namespace httpWebRequestLab
                 });
         }
 
-        //private void btnGo_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        this.Cursor = Cursors.WaitCursor;
-        //
-        //        //# prefix actions
-        //        txtOutput.Clear();
-        //
-        //        //
-        //        string url = cboUrl.Text.Trim();
-        //        txtMessage.AppendText("\r\n### ### ### ### ### ###\r\n");
-        //        txtMessage.AppendText(string.Format("URL : {0}\r\n", url));
-        //
-        //        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-        //
-        //        // Set some reasonable limits on resources used by this request
-        //        request.MaximumAutomaticRedirections = 4;
-        //        request.MaximumResponseHeadersLength = 4;
-        //        request.Timeout = (int)numTimeOut.Value; // milliseconds
-        //
-        //        // Set credentials to use for this request.
-        //        request.Credentials = CredentialCache.DefaultCredentials;
-        //        request.AuthenticationLevel = (AuthenticationLevel)cbxAuthLevel.SelectedValue; // AuthenticationLevel.None;
-        //        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-        //
-        //        // Show some properties
-        //        txtMessage.AppendText(string.Format("Content Length : {0:N0}\r\n", response.ContentLength));
-        //        txtMessage.AppendText(string.Format("Content Type : {0}\r\n", response.ContentType));
-        //        txtMessage.AppendText(string.Format("Content Encoding : {0}\r\n", response.ContentEncoding));
-        //        txtMessage.AppendText(string.Format("Is mutually authenticated? {0}\r\n", response.IsMutuallyAuthenticated));
-        //
-        //        // Pipes the stream to a higher level stream reader with the required encoding format. 
-        //        StreamReader readStream = new StreamReader(response.GetResponseStream(), Encoding.ASCII);
-        //        txtMessage.AppendText("Response stream received.\r\n");
-        //
-        //        // show URL content
-        //        txtOutput.AppendText(readStream.ReadToEnd());
-        //
-        //        // check is match pattern or not with 10 seconds timeout.
-        //        if (Regex.IsMatch(txtOutput.Text, txtPattern.Text, RegexOptions.None, new TimeSpan(0, 0, 10)))
-        //            txtMessage.AppendText("Match the RegEx pattern.\r\n");
-        //        else
-        //            txtMessage.AppendText("NOT match the RegEx pattern.\r\n");
-        //
-        //        response.Close();
-        //        readStream.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        txtMessage.AppendText("\r\n ===>>><<<===\r\n");
-        //        txtMessage.AppendText(string.Format("EXCEPTION : {0}\r\n", ex.GetType().Name));
-        //        txtMessage.AppendText(string.Format("MESSAGE : {0}\r\n", ex.Message));
-        //    }
-        //    finally
-        //    {
-        //        this.Cursor = Cursors.Default;
-        //    }
-        //}
     }
 }
